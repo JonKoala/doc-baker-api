ac033423783ae31b86e9e275bc38bb80434818e5
JonKoala <victor.santana.lemos@gmail.com>
2018-09-04 20:04:24 -0300

    simplifica controle de requisitos presentes

--- models/Processo.documento.mtp.js
+++ models/Processo.documento.mtp.js
@@ -3,10 +3,7 @@ const Schema = require('mongoose').Schema
 
 module.exports = {
   auditores: [{ type: Schema.Types.ObjectId, ref: 'Auditor' }],
-  requisitos: {
-    presentes: [{ type: Schema.Types.ObjectId, ref: 'RequisitoAdmissibilidade' }],
-    ausentes: [{ type: Schema.Types.ObjectId, ref: 'RequisitoAdmissibilidade' }]
-  },
+  requisitosPresentes: [{ type: Schema.Types.ObjectId, ref: 'RequisitoAdmissibilidade' }],
   pressupostos: {
     presenteFumus: Boolean,
     presentePericulum: { type: String, enum: ['SIM', 'NÃO', 'REVERSO'] }
