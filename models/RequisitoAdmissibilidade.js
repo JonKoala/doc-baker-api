c11753b0859dbe0e18e15392e77df76b0d43ac97
JonKoala <victor.santana.lemos@gmail.com>
2018-09-14 19:13:25 -0300

    adiciona descricao generica

--- models/RequisitoAdmissibilidade.js
+++ models/RequisitoAdmissibilidade.js
@@ -6,7 +6,8 @@ const Schema = mongoose.Schema
 mongoose.model('RequisitoAdmissibilidade', new mongoose.Schema({
   inciso: String,
   descricao: {
+    ausente: String,
+    generico: String,
     presente: String,
-    ausente: String
   }
 }, { collection: 'requisitosadmissibilidade' }))
